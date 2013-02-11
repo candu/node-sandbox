9c6dbc03eb0ac0dbd4232c5fca3ccad417d72dc9
Evan Savage <savage.evan@gmail.com>
2013-02-11 11:32:10 -0800

    Quick express.js test
    with jade views and layout.

--- project70/beginner-tutorial/ExpressTest.js
+++ project70/beginner-tutorial/ExpressTest.js
@@ -1,6 +1,27 @@
-var express = require('express');
+var express = require('express'),
+    path = require('path');
+
 var app = express();
-app.get('/', function(req, res) {
-  res.send('Hello World');
+
+app.configure('development', function() {
+  app.use(express.logger());
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
 });
+
+app.configure('production', function() {
+  app.use(express.logger());
+  app.use(express.errorHandler());
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  //throw new Error('My error here');
+  res.render('root');
+});
+
 app.listen(3000);
