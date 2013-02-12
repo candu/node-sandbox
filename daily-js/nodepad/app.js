5ac2e0cbcbf23d1f0cd5e92c5b9e49b2c04b8a5d
Evan Savage <savage.evan@gmail.com>
2013-02-12 14:49:53 -0800

    Finally got session login working properly
    I keep running into the same problem with Mongoose: I'm following an old
    tutorial that has callbacks like
    
        function(user) {
          // do stuff...
        }
    
    which of course fails when it should succeed, because the correct
    callback form is the node.js standard
    
        function(err, user) {
          // do stuff...
        }
    
    On success, `null` is passed as the first argument. Oops.

--- daily-js/nodepad/app.js
+++ daily-js/nodepad/app.js
@@ -1,10 +1,11 @@
 var express = require('express'),
+    MongoStore = require('connect-mongo')(express),
     stylus = require('stylus'),
     http = require('http'),
     path = require('path'),
     mongoose = require('mongoose'),
     _ = require('../../lib/underscore'),
-    Document = require('models').Document;
+    models = require('models');
 
 var app = express();
 
@@ -25,12 +26,9 @@ app.configure(function() {
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler());
 });
 
@@ -41,20 +39,88 @@ app.configure('test', function() {
 
 app.configure(function() {
   console.log('connecting to DB: ' + app.get('db'));
-  mongoose.connect('mongodb://localhost/' + app.get('db'));
+  app.set('db-uri', 'mongodb://localhost/' + app.get('db'));
+  var db = mongoose.connect(app.get('db-uri'))
+      store = new MongoStore({db: db.connections[0].db});
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: 'sooperseekret',
+    store: store
+  }));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/', function(req, res) {
+function loadUser(req, res, next) {
+  if (req.session.userID) {
+    var _id = mongoose.Types.ObjectId(req.session.userID);
+    models.User.findById(_id, function(err, user) {
+      if (user) {
+        req.currentUser = user;
+        next();
+      } else {
+        res.redirect('/sessions/new');
+      }
+    });
+  } else {
+    res.redirect('/sessions/new');
+  }
+}
+
+app.get('/sessions/new', function(req, res) {
+  res.render('sessions/new', {user: new models.User()});
+});
+
+app.post('/sessions', function(req, res) {
+  models.User.findOne({email: req.body.user.email}, function(err, user) {
+    if (user && user.authenticate(req.body.user.password)) {
+      req.session.userID = user.id;
+      res.redirect('/documents');
+    } else {
+      res.redirect('/sessions/new');
+    }
+  });
+});
+
+app.del('/sessions', loadUser, function(req, res) {
+  if (req.session) {
+    req.session.destroy(function() {});
+  }
+  res.redirect('/sessions/new');
+});
+
+app.get('/users/new', function(req, res) {
+  res.render('users/new', {user: new models.User()});
+});
+
+app.post('/users.:format?', function(req, res) {
+  var data = req.body.user;
+  if (_.isString(data)) {
+    data = JSON.parse(data);
+  }
+  var user = new models.User(data);
+  user.save(function() {
+    switch (req.params.format) {
+      case 'json':
+        res.send(user);
+        break;
+      default:
+        req.session.userID = user.id;
+        res.redirect('/documents');
+    }
+  });
+});
+
+app.get('/', loadUser, function(req, res) {
   res.send('hello there!');
 });
 
-app.get('/documents.:format?', function(req, res) {
-  Document.find(function(err, documents) {
+app.get('/documents.:format?', loadUser, function(req, res) {
+  models.Document.find(function(err, documents) {
     switch (req.params.format) {
       case 'json':
-        res.send(documents.map(function(d) {
-          return d.__doc;
-        }));
+        res.send(documents);
         break;
       default:
         res.render('documents/index', {documents: documents});
@@ -62,12 +128,12 @@ app.get('/documents.:format?', function(req, res) {
   });
 });
 
-app.post('/documents.:format?', function(req, res) {
+app.post('/documents.:format?', loadUser, function(req, res) {
   var data = req.body.document;
   if (_.isString(data)) {
     data = JSON.parse(data);
   }
-  var d = new Document(data);
+  var d = new models.Document(data);
   d.save(function() {
     switch (req.params.format) {
       case 'json':
@@ -79,19 +145,19 @@ app.post('/documents.:format?', function(req, res) {
   });
 });
 
-app.get('/documents/:id.:format?/edit', function(req, res) {
-  Document.findById(req.params.id, function(err, d) {
+app.get('/documents/:id.:format?/edit', loadUser, function(req, res) {
+  models.Document.findById(req.params.id, function(err, d) {
     // TODO: error handling
     res.render('documents/edit', {d: d});
   });
 });
 
-app.get('/documents/new', function(req, res) {
-  res.render('documents/new', {d: new Document() });
+app.get('/documents/new', loadUser, function(req, res) {
+  res.render('documents/new', {d: new models.Document() });
 });
 
-app.get('/documents/:id.:format?', function(req, res) {
-  Document.findById(req.params.id, function(err, d) {
+app.get('/documents/:id.:format?', loadUser, function(req, res) {
+  models.Document.findById(req.params.id, function(err, d) {
     switch (req.params.format) {
       case 'json':
         res.send(d);
@@ -102,8 +168,8 @@ app.get('/documents/:id.:format?', function(req, res) {
   });
 });
 
-app.put('/documents/:id.:format?', function(req, res) {
-  Document.findById(req.body.document.id, function(err, d) {
+app.put('/documents/:id.:format?', loadUser, function(req, res) {
+  models.Document.findById(req.body.document.id, function(err, d) {
     d.title = req.body.document.title;
     d.data = req.body.document.data;
     d.save(function() {
--- daily-js/nodepad/models.js
+++ daily-js/nodepad/models.js
@@ -1,4 +1,6 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    crypto = require('crypto'),
+    uuid = require('node-uuid');
 
 var DocumentSchema = mongoose.Schema({
   title: String,
@@ -6,5 +8,32 @@ var DocumentSchema = mongoose.Schema({
   tags: [String]
 });
 DocumentSchema.index({title: 1});
-var Document = mongoose.model('Document', DocumentSchema);
-exports.Document = Document;
+
+function normalizeEmail(email) {
+  return email.toLowerCase();
+}
+
+function hashPassword(password) {
+  if (!this.salt) {
+    this.salt = uuid.v4();
+  }
+  return this.encryptPassword(password);
+}
+
+var UserSchema = mongoose.Schema({
+  email: {type: String, set: normalizeEmail},
+  salt: String,
+  password: {type: String, set: hashPassword}
+});
+UserSchema.index({email: 1}, {unique: true});
+UserSchema.methods.encryptPassword = function(password) {
+  return crypto.createHmac('sha1', this.salt)
+    .update(password)
+    .digest('hex');
+};
+UserSchema.methods.authenticate = function(password) {
+  return this.password === this.encryptPassword(password);
+};
+
+exports.Document = mongoose.model('Document', DocumentSchema);
+exports.User = mongoose.model('User', UserSchema);
