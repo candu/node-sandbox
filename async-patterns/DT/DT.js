7a48fbad0080ed3917811ba390a71f71d2b35610
Evan Savage <savage.evan@gmail.com>
2013-05-01 09:30:35 +0200

    working with multifetch
    except that this will currently leak memory in the CallGraph, which
    never cleans itself up.

--- async-patterns/DT/DT.js
+++ async-patterns/DT/DT.js
@@ -52,7 +52,6 @@ AbstractDataType.prototype.dispatch = function() {
     var key = this.cacheKey(id);
     this._cache[key] = values[id];
   }.bind(this));
-  console.log(this._cache);
 };
 AbstractDataType.prototype.gen = function(ids) {
   var fiber = Fiber(function() {
@@ -63,7 +62,7 @@ AbstractDataType.prototype.gen = function(ids) {
     _.each(ids, function(id) {
       this._idsToFetch[id] = true;
     }.bind(this));
-    Fiber.yield(null);
+    Fiber.yield(needDispatch());
     console.log('gen after fetch: ', ids);
     var objs = {};
     _.each(ids, function(id) {
@@ -93,15 +92,21 @@ function Result(value) {
   this.value = value;
 }
 
-function wait(gen) {
-  var parent = Fiber.current;
-  Dispatcher.wait(gen, parent);
+function needDispatch() {
+  console.log('needDispatch');
+  var gen = Fiber.current;
+  Dispatcher.wait(gen, null);
 }
 
-function waitv(/* gen1, ..., genN */) {
-  var parent = Fiber.current,
-      gens = Array.prototype.slice.call(arguments);
-  Dispatcher.waitv(gens, parent);
+function wait(waitGen) {
+  var gen = Fiber.current;
+  Dispatcher.wait(gen, waitGen);
+}
+
+function waitv(/* waitGen1, ..., waitGenN */) {
+  var gen = Fiber.current,
+      waitGens = Array.prototype.slice.call(arguments);
+  Dispatcher.wait(gen, waitGens);
 }
 
 function result(value) {
@@ -111,109 +116,170 @@ function result(value) {
 var App = Fiber(function App() {
   var user = Fiber.yield(wait(DT('Obj').gen(1)));
   console.log('user: ', user);
-  /*
   var users = Fiber.yield(waitv(
     DT('Obj').gen(1),
     DT('Obj').gen(2)
   ));
-  console.log(users);
-  */
+  console.log('users: ', users);
   Fiber.yield(result(user));
 });
 
-var Dispatcher = {
-  _gens: [],
-  _waits: {},
-  _results: {}
+var NodeType = {
+  LEAF: 1,
+  WAIT: 2,
+  WAITV: 3
 };
-Dispatcher.dispatch = function() {
-  DT.dispatch();
+
+function CallGraphNode(id, waitIds) {
+  this._id = id;
+  this._waitIds = {};
+  if (waitIds === null) {
+    this._type = NodeType.LEAF;
+  } else if (!(waitIds instanceof Array)) {
+    this._type = NodeType.WAIT;
+    this._waitIds[waitIds] = true;
+  } else {
+    this._type = NodeType.WAITV;
+    _.each(waitIds, function(waitId) {
+      this._waitIds[waitId] = true;
+    }.bind(this));
+  }
+  this._result = null;
+  this._hasResult = false;
+}
+CallGraphNode.prototype.type = function() {
+  return this._type;
 };
-Dispatcher._genId = function(gen) {
+CallGraphNode.prototype.waitIds = function() {
+  return _.keys(this._waitIds);
+};
+CallGraphNode.prototype.setResult = function(result) {
+  this._result = result;
+  this._hasResult = true;
+};
+CallGraphNode.prototype.hasResult = function() {
+  return this._hasResult;
+};
+CallGraphNode.prototype.result = function() {
+  return this._result;
+};
+
+function CallGraph() {
+  this._gens = [];
+  this._nodes = {};
+  this._result = null;
+  this._hasResult = false;
+}
+CallGraph.prototype.id = function(gen) {
   if (!_.contains(this._gens, gen)) {
     this._gens.push(gen);
   }
   return _.indexOf(this._gens, gen);
 };
-Dispatcher._gen = function(genId) {
+CallGraph.prototype.gen = function(genId) {
   return this._gens[genId];
 };
-Dispatcher._canProceed = function(genId) {
-  var waitIds = this._waits[genId];
-  if (!waitIds) {
-    return true;
+CallGraph.prototype.setNode = function(gen, waitGens) {
+  var genId = this.id(gen);
+  this._nodes[genId] = new CallGraphNode(genId, null);
+  if (waitGens === null) {
+    this._nodes[genId] = new CallGraphNode(genId, waitGens);
+  } else if (!(waitGens instanceof Array)) {
+    var waitId = this.id(waitGens);
+    this._nodes[genId] = new CallGraphNode(genId, waitId);
+  } else {
+    var waitIds = _.map(waitGens, this.id.bind(this));
+    this._nodes[genId] = new CallGraphNode(genId, waitIds);
   }
-  if (!(waitIds instanceof Array)) {
-    waitIds = [waitIds];
+};
+CallGraph.prototype.setResult = function(gen, result) {
+  var genId = this.id(gen);
+  this._nodes[genId].setResult(result);
+};
+CallGraph.prototype.hasResult = function(gen) {
+  var genId = this.id(gen);
+  return this._nodes[genId].hasResult(result);
+};
+CallGraph.prototype.result = function(gen) {
+  var genId = this.id(gen);
+  return this._nodes[genId].result(result);
+};
+CallGraph.prototype.getRunnableIds = function() {
+  var runnableIds = {};
+  _.each(this._nodes, function(node, genId) {
+    var waitIds = node.waitIds();
+    var waitGensFinished = _.every(waitIds, function(waitId) {
+      return this._nodes[waitId].hasResult();
+    }.bind(this));
+    if (waitGensFinished && !node.hasResult()) {
+      runnableIds[genId] = true;
+    }
+  }.bind(this));
+  console.log('runnableIds:', runnableIds);
+  return _.keys(runnableIds);
+};
+CallGraph.prototype.getSendValue = function(gen) {
+  console.log('getSendValue:', gen);
+  var genId = this.id(gen),
+      node = this._nodes[genId],
+      waitIds = node.waitIds();
+  if (node.type() === NodeType.WAIT) {
+    var waitNode = this._nodes[waitIds[0]];
+    return waitNode.result();
   }
-  return _.every(waitIds, function hasResult(waitId) {
-    return _.has(this._results, waitId);
+  return _.map(waitIds, function(waitId) {
+    var waitNode = this._nodes[waitId];
+    return waitNode.result();
   }.bind(this));
 };
-Dispatcher._getSendValue = function(genId) {
-  var waitIds = this._waits[genId];
-  if (!(waitIds instanceof Array)) {
-    return this._results[waitIds];
-  }
-  return _.map(waitIds, function fetchResult(waitId) {
-    return this._results[waitId];
-  });
+CallGraph.prototype.isTrivial = function() {
+  return _.size(this._nodes) === 1;
 };
-Dispatcher.wait = function(gen, parent) {
-  var genId = this._genId(gen),
-      parentId = this._genId(parent);
-  this._waits[parentId] = genId;
-  var yielded = gen.run();
-  if (yielded instanceof Result) {
-    this._results[genId] = yielded.value;
-  }
+
+var Dispatcher = {
+  _graph: new CallGraph()
 };
-Dispatcher.waitv = function(gens, parent) {
-  var genIds = _.map(gens, this._genId.bind(this)),
-      parentId = this._genId(parent);
-  this._waits[parentId] = genIds;
-  _.each(gens, function(gen, i) {
-    var genId = genIds[i],
-        yielded = gen.run();
-    if (yielded instanceof Result) {
-      this._results[genId] = yielded.value;
-    }
-  });
+Dispatcher.dispatch = function() {
+  DT.dispatch();
+};
+Dispatcher.wait = function(gen, waitGens) {
+  this._graph.setNode(gen, waitGens);
+  if (waitGens === null) {
+  } else if (!(waitGens instanceof Array)) {
+    waitGens.run();
+  } else {
+    _.each(waitGens, function(waitGen) {
+      waitGen.run();
+    });
+  }
 };
 Dispatcher.run = function(root) {
-  this.wait(root, null);
-  while (!_.isEmpty(this._waits)) {
-    // first, determine which generators to run
-    var toRun = {};
-    _.each(this._waits, function(genIds, parentId) {
-      if (!(genIds instanceof Array)) {
-        genIds = [genIds];
-      }
-      _.each(genIds, function(genId) {
-        if (this._canProceed(genId)) {
-          toRun[genId] = this._getSendValue(genId);
-        }
-      }.bind(this));
-    }.bind(this));
-    // next, run each of those generators one step
-    _.each(toRun, function(sent, genId) {
-      var gen = this._gen(genId),
-          yielded = gen.run(sent);
-      if (yielded instanceof Result) {
-        console.log('result: ', yielded);
-        this._results[genId] = yielded.value;
-        delete this._waits[genId];
-      }
-    }.bind(this));
-    // finally, dispatch to backend data fetchers
+  this.wait(Fiber.current, root);
+  while (!this._graph.hasResult(Fiber.current)) {
+    // first, dispatch
     this.dispatch();
+    // next, run generators
+    var runnable = this._graph.getRunnableIds();
+    console.log('runnable:', runnable);
+    _.each(runnable, function(genId) {
+      console.log('genId:', genId);
+      var gen = this._graph.gen(genId),
+          sendValue = this._graph.getSendValue(gen);
+      if (gen === Fiber.current) {
+        this._graph.setResult(gen, sendValue);
+      } else {
+        var yielded = gen.run(sendValue);
+        if (yielded instanceof Result) {
+          console.log('result: ', yielded);
+          this._graph.setResult(gen, yielded.value);
+        }
+      }
+    }.bind(this));
   }
+  return this._graph.result(Fiber.current);
 };
 
-// TODO: when do you delete entries in this._waits?
-//
-// a generator has finished running when it results a result. that is it.
-
 Fiber(function main() {
-  Dispatcher.run(App);
+  var result = Dispatcher.run(App);
+  console.log(result);
 }).run();
